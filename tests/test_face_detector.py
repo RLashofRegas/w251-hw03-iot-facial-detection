80d0c713c1b4d0be3ddc888dfba52ee85408e6fb
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2020-05-15 21:52:57 -0700

    add linting and fix all errors

--- tests/test_face_detector.py
+++ tests/test_face_detector.py
@@ -1,9 +1,14 @@
+"""Tests for the face_detector package."""
 import unittest
 
-class TestFaceDetector(unittest.TestCase):
 
-    def test_detect_face(self):
+class TestFaceDetector(unittest.TestCase):
+    """Tests for the face_detector module."""
+
+    def test_detect_faces(self) -> None:
+        """Test that faces are detected in sample image."""
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
