6c2fe8082261b9accdcda5b77da9d8c5c6a06024
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2020-05-25 19:29:19 -0700

    fix credential env variable

--- cloud_server/docker_up.sh
+++ cloud_server/docker_up.sh
@@ -1,3 +1,3 @@
 export API_KEY=$( jq ".[0].credentials.apikey" /root/.bluemix/cos_credentials )
-export CRN=$( jq ".[0].crn" /root/.bluemix/cos_credentials )
+export CRN=$( jq ".[0].credentials.resource_instance_id" /root/.bluemix/cos_credentials )
 docker-compose up --build
